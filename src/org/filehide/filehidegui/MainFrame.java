060cdf89a8c3871d27fb7f5425df35a863c9d195
Alexis <alexis@thecoolguy.com>
2017-02-21 00:33:22 +0100

    Mainly using lambdas now.
    Also cleanup and visibility changes.

--- src/org/filehide/filehidegui/MainFrame.java
+++ src/org/filehide/filehidegui/MainFrame.java
@@ -1,21 +1,19 @@
 package org.filehide.filehidegui;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.WindowEvent;
-import java.util.ArrayList;
+
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.KeyStroke;
 import javax.swing.JLabel;
-import javax.swing.JTextPane;
 
+@SuppressWarnings("serial")
 public class MainFrame extends JFrame  {
+	
 	// TODO remove this var
 	static MainFrame mainFrame;
 	
@@ -25,66 +23,49 @@ public class MainFrame extends JFrame  {
 	
 	
 	// Fields
-	
-	JMenuItem[][] menuItems = {
-			{
-			new JMenuItem("Extract") {{
-				addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-						new FHExtractFile();
-					}
-				});
-			}},
-			new JMenuItem("Retrieve") {{
-				addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-						new FHRetrieveFile();
-					}
-				});
-			}},
-			new JMenuItem("Quit") {{
-				addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-						System.exit(0);
-					}
-				});
-			}}
-			},
-			{
-			new JMenuItem("About") {{
-				addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-						new AboutFrame();
-					}
-				});
-			}}
-			}
-	};
-	
-	JMenu[] menues = {
+
+	private JMenu[] menues = {
 			new JMenu("File"),
 			new JMenu("Help")
 	};
 	
-	List<List<Integer>> seperators = Arrays.asList(Arrays.asList(1), Arrays.asList());
-
+	private JMenuItem[][] menuItems = {
+			{
+				new JMenuItem("Extract") {{
+					addActionListener(e -> new FHExtractFile());
+				}},
+				new JMenuItem("Retrieve") {{
+					addActionListener(e -> new FHRetrieveFile());
+				}},
+				new JMenuItem("Quit") {{
+					addActionListener(e -> System.exit(0));
+				}}
+			},
+			{
+				new JMenuItem("About") {{
+					addActionListener(e -> new AboutFrame());
+				}}
+			}
+	};
 	
-	JTextPane txtpnAboutAlexisUnd;
+	private List<Collection<Integer>> seperators = Arrays.asList(
+			new HashSet<Integer>(Arrays.asList(1)),
+			new HashSet<Integer>()
+	);
 	
-	public MainFrame()
-	{
+	private MainFrame() {
+		super("FileHide");
 		this.setSize(500, 500);
 		this.setResizable(false);
-		this.setTitle("FileHide");
 		
 		JMenuBar menuBar = new JMenuBar();
 
-		for(int i = 0; i < menues.length; i++) {
-			menuBar.add(menues[i]);
-			for(int j = 0; j < menuItems[i].length; j++) {
-				menues[i].add(menuItems[i][j]);
-				if(seperators.get(i).contains(j)) {
-					menues[i].addSeparator();
+		for(int i = 0; i < this.menues.length; i++) {
+			menuBar.add(this.menues[i]);
+			for(int j = 0; j < this.menuItems[i].length; j++) {
+				this.menues[i].add(this.menuItems[i][j]);
+				if(this.seperators.get(i).contains(j)) {
+					this.menues[i].addSeparator();
 				}
 			}
 		}
@@ -97,4 +78,5 @@ public class MainFrame extends JFrame  {
 		
 		this.setVisible(true);
 	}
+
 }
